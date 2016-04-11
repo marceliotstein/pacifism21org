eee326e4f78b46b1125f4e2c30862d6eb901522d
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2016-04-11 12:28:24 -0400

    fixes to blog front styling

--- sites/all/themes/pactober/templates/views-view-fields--front-more-items.tpl.php
+++ sites/all/themes/pactober/templates/views-view-fields--front-more-items.tpl.php
@@ -3,14 +3,29 @@
 ?>
 <?php
 $title = "";
+$body = "";
+$type = "";
 foreach ($fields as $id => $field) {
   if ($id=="title") {
     $title = $field->content;
+  } else if ($id=="body") {
+    $body = $field->content;
+  } else if ($id=="type") {
+    $type = $field->content;
   }
 }
 ?>
-<div class="well">
-  <div class="pac-blog-title-img" style="background-image: url(sites/all/themes/pactober/images/ptfromtheblog1200.jpg)">
-    <?php print $title ?>
+<?php if ($type=="Item"): ?>
+  <div class="well">
+    <div class="pac-blog-title-img" style="background-image: url(sites/all/themes/pactober/images/ptfromtheblog1200.jpg)">
+      <?php print $title ?>
+    </div>
   </div>
-</div>
+<?php else: ?>
+  <div class="well">
+    <div class="pac-tout-front img-responsive">
+      <div class="pac-blog-title"><?php print $title ?></div>
+      <?php print $body ?>
+    </div>
+  </div>
+<?php endif; ?>
